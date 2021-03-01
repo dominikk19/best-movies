2ed3248c4b7f3334554402000684637cdfa2ea80
dominikk19 <dominikk19@gmail.com>
2021-03-01 18:03:06 +0100

    added project structure

---
+++ angular-app/src/app/pages/categories/movies-in-category/movies-in-category.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-movies-in-category',
+  templateUrl: './movies-in-category.component.html',
+  styles: [
+  ]
+})
+export class MoviesInCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-app/src/app/pages/movies/movies-details/movies-details.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-movies-details',
+  templateUrl: './movies-details.component.html',
+  styles: [
+  ]
+})
+export class MoviesDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
